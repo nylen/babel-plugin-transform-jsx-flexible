d946e9b5d24b25e2bcc52bf4883f16c625fc084e
James Nylen <jnylen@gmail.com>
2017-05-23 23:49:36 -0400

    Make the `babel-plugin-transform-react-jsx` tests work

--- test/index.js
+++ test/index.js
@@ -1,3 +1,28 @@
+// Enable "transform-jsx-flexible" to work in options.json files
+// TODO find a better way than this hack
+import fs from 'fs';
+import path from 'path';
+
+const dirname = path.join(
+	__dirname,
+	'..',
+	'node_modules',
+	'babel-plugin-transform-jsx-flexible'
+);
+try {
+	fs.mkdirSync( dirname );
+} catch ( err ) { }
+try {
+	fs.unlinkSync( path.join( dirname, 'index.js' ) );
+} catch ( err ) { }
+
+fs.symlinkSync(
+	'../../index.js',
+	path.join( dirname, 'index.js' )
+);
+
+// Now run the regular babel testing logic
+
 import runner from "babel-helper-plugin-test-runner";
 
 runner(__dirname);
